298aeb19698f50b756903a714cd26183e2e85d98
bspotswood <bspotswood@users.noreply.github.com>
2025-05-06 23:31:14 +0000

    deploy: a8c15d41ad47a9ecbf3b7a87d34835cda3661a35

--- _data/metadata.js
+++
@@ -1,9 +0,0 @@
-export default {
-	title: "Journal Worthy Journey",
-	url: "https://journalworthyjourney.com",
-	language: "en",
-	description: "Full Time RV couple sharing our travels living in our Keystone Montana 3813ms 5th wheel.",
-	author: {
-		name: "Brent and Tayler Spotswood"
-	}
-}
